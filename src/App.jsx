e3c76c835471e056e5882a6b76b64704738a728c
om-dev007 <officialomnarayan@gmail.com>
2025-12-03 00:55:33 +0530

    upt

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
 
 const App = () => {
-  useEffect(() => {
-    
-    const getData = async () => {
-      const data = await axios.get('https://fakestoreapi.com/products')
-      console.log(data.data);
-    }
-
-    getData()
-  }, [])
+ 
   return (
-    <div className='bg-gray-900 h-screen'>App</div>
+    <div className='bg-gray-900 min-h-screen text-white'>App</div>
   )
 }
 
