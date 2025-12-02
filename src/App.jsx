76393b664517b0cbe4ad80e7c7767c2c132ea713
om-dev007 <officialomnarayan@gmail.com>
2025-12-03 01:31:40 +0530

    nb

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Home from "./pages/Home"
 
 const App = () => {
  
   return (
-    <div className='bg-gray-900 min-h-screen text-white'>App</div>
+    <div className='bg-gray-900 min-h-screen text-white'>
+      <Home/>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <h1 className='font-bold text-2xl '>SHOP.CO</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
