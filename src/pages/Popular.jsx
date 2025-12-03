36ccc69e19274913e953f2e3004c4b2aa69b117c
om-dev007 <officialomnarayan@gmail.com>
2025-12-04 01:46:18 +0530

    popular added

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import Cards from '../components/Cards'
-import data from '../store/data'
+import { data } from '../store/data'
 
 const Popular = () => {
 
   return (
     <div className='my-5'>
         <div>
-            <h1 className='text-4xl text-center font-bold'>POPULAR IN WOMEN</h1>
+            <h1 className='text-4xl text-center font-bold'>POPULAR IN TRENDING</h1>
         </div>
         <div>
-            <hr className='w-60 bg-gray-900 h-1 rounded-2xl flex justify-self-center font-extrabold' />
+            <hr className='w-80 bg-gray-900  h-1 rounded-2xl flex justify-self-center font-extrabold' />
         </div>
-        <div>
-            {data.map(() => {
-                return (<Cards data={data} />)
+        <div className='flex flex-wrap gap-2 py-5 px-5 justify-evenly'>
+            {data.map((item) => {
+                return (<Cards key={item.id} data={item} />)
             })}
         </div>
         
