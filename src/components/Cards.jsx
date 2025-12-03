a28ae4f840e01c833a888f3a01c40608ca293df1
om-dev007 <officialomnarayan@gmail.com>
2025-12-04 01:45:59 +0530

    card done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 const Cards = ({data}) => {
     console.log(data);
-    
-    return (
-        <div className='cards rounded-2xl w-50 h-100'>
-            <div className=''>
-                <img className='w-50 object-contain rounded-2xl' src={salvar} alt="" />
-            </div>
-            <div>
 
+    return (
+        <div className='cards rounded-2xl w-50 h-auto py-2'>
+            <div className='object-contain'>
+                <img className='w-50 h-50 object-contain rounded-4xl' src={data.image} alt={data.title} />
+            </div>
+            <div className='text-center py-1'>
+                <h3 className='py-1'>{data.title}</h3>
+                <p>Rating: {data.rating}</p>
             </div>
         </div>
     )
