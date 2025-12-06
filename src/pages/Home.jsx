5cb9acdfeda21e509f6b9e3189108e23ac8e52ba
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 16:36:14 +0530

    Home upt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import ProductCard from '../components/ProductCard'
 import Product from './Product'
 import Popular from './Popular'
 import OfferPage from './OfferPage'
+import NewCollection from './NewCollection'
 
 const Home = () => {
   return (
@@ -50,6 +51,7 @@ const Home = () => {
       </div>
       <Popular/>
       <OfferPage/>
+      <NewCollection/>
     </>
   )
 }
