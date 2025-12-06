4ae33581e6082c3b0efc88cd8f15cc8299e86378
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 16:50:29 +0530

    tailwind

--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -4,7 +4,7 @@ import offerGirl from '../assets/offerGirl.webp'
 const OfferPage = () => {
   return (
     <div className='flex justify-center px-50 py-30'>
-        <div className='bg-linear-to-t from-emerald-400 to-white items-center w-[100%] justify-evenly rounded-2xl flex py-10 px-30'>
+        <div className='bg-linear-to-t from-emerald-400 to-white items-center w-full justify-evenly rounded-2xl flex py-10 px-30'>
             <div>
                 <h1 className='text-4xl font-semibold'>Exclusive</h1>
                 <h1 className='text-4xl font-semibold'>Offers For You</h1>
