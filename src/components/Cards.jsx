aa46049709e96bfaafab7ce24a66cbda9991325e
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 14:23:25 +0530

    cards updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Cards = ({data}) => {
+const Cards = ({ data }) => {
     console.log(data);
 
     return (
-        <div className='cards rounded-2xl border-2 border-gray-400 shadow-gray-400 w-50 h-auto py-2'>
-            <div className='object-contain'>
-                <img className='w-50 h-50 object-contain ' src={data.image} alt={data.title} />
+        <div className="cards rounded-2xl py-2 border border-gray-300 bg-white transition-all duration-300 ease-in-out hover:translate-y-2 hover:shadow-2xl">
+            <div className="object-contain">
+                <img
+                    className="w-50 h-50 rounded-2xl object-contain "
+                    src={data.image}
+                    alt={data.title}
+                />
             </div>
-            <div className='text-center py-1'>
-                <h3 className='py-1'>{data.title}</h3>
+            <div className="text-center py-1">
+                <h3 className="py-1">{data.title}</h3>
                 <p>Rating: {data.rating}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Cards
+export default Cards;
