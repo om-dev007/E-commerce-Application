96f35615407bf95a6b38c5dab60e0ea08cf8e25e
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 16:43:27 +0530

    new collec

--- src/pages/NewCollection.jsx
+++ src/pages/NewCollection.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Cards from '../components/Cards'
+import { collectionData } from '../store/data'
 
 const NewCollection = () => {
   return (
-    <div>NewCollection</div>
+    <div>
+      <div>
+        <h1 className='text-center font-bold text-4xl'>NEW COLLECTIONS</h1>
+        <hr className='w-50 bg-gray-900  h-1 rounded-2xl flex justify-self-center font-extrabold' />
+      </div>
+      <div className='flex flex-wrap py-10 px-10 justify-center gap-7'>
+        {
+          collectionData.map((items) => {
+            return <Cards data={items} key={items.id} />
+          })
+        }
+      </div>
+    </div>
   )
 }
 
