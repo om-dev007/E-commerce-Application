1bb6d83b4550b8108a5cf96ff799548c430d8dd9
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 16:36:27 +0530

    upted popu

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -10,9 +10,9 @@ const Popular = () => {
             <h1 className='text-4xl text-center font-bold'>POPULAR IN TRENDING</h1>
         </div>
         <div>
-            <hr className='w-80 bg-gray-900  h-1 rounded-2xl flex justify-self-center font-extrabold' />
+            <hr className='w-50 bg-gray-900  h-1 rounded-2xl flex justify-self-center font-extrabold' />
         </div>
-        <div className='flex flex-wrap gap-7 py-5 px-5 justify-center'>
+        <div className='flex flex-wrap gap-7 py-10 px-5 justify-center'>
             {data.map((item) => {
                 return (<Cards key={item.id} data={item} />)
             })}
