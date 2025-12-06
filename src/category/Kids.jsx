2896effbe7759b49c7ad32999bf8e22a21fff912
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 20:42:31 +0530

    kids

--- src/category/Kids.jsx
+++ src/category/Kids.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import kidsSale from '../assets/kidsSale.jpg'
+import Navbar from '../components/Navbar'
 
 const Kids = () => {
   return (
-    <div>Kids</div>
+    <>
+    <Navbar/>
+      <div>
+        <div className='flex px-10 justify-center py-2'>
+          <img className='w-[85%] rounded ' src={kidsSale} alt="" />
+        </div>
+      </div>
+    </>
   )
 }
 
