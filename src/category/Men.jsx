c1e93523f1198a2ab02f26dd15e1240646538dc1
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 19:39:38 +0530

    updated men

--- src/category/Men.jsx
+++ src/category/Men.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import menHero from '../assets/menHero.jpg'
+import { menData } from '../store/data'
+import Cards from '../components/Cards'
+import Footer from '../components/Footer'
 
 const Men = () => {
-  return (
-    <div>
-        <Navbar/>
-        <div className=''>
-            <img src={menHero} alt="" />
-        </div>
-    </div>
-  )
+    return (
+        <>
+            <Navbar />
+            <div className='py-2 flex justify-center px-10'>
+                <img className='w-[85%] rounded ' src={menHero} alt="" />
+            </div>
+            <div className='flex gap-5 justify-center flex-wrap py-10 px-15'>
+                {menData.map((items) => {
+                    return <Cards data={items} key={items.id} />
+                })}
+            </div>
+            <Footer/>
+        </>
+    )
 }
 
 export default Men
