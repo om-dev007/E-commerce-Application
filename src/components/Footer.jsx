c7ea3baab002387377348199ef6872cfc4061408
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 18:05:58 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import logo from '../assets/logo (1).webp'
+import { Link } from 'react-router-dom'
+import whatsapp from '../assets/whatsapp.png'
+import facebook from '../assets/facebook.png'
+import instagram from '../assets/instagram.png'
+
+const Footer = () => {
+    return (
+        <>
+            <div className='flex justify-self-center gap-5 flex-col  px-10'>
+                <div className='flex py-5 justify-center gap-5 items-center'>
+                    <div>
+                        <img className='w-10' src={logo} alt="" />
+                    </div>
+                    <div>
+                        <h3 className='text-4xl text-[#454545] font-bold '>Shopify</h3>
+                    </div>
+                </div>
+                <div className='flex gap-5 text-gray-700 font-medium text-lg '>
+                    <Link to='/about' >About</Link>
+                    <Link to='/products' >Products</Link>
+                    <Link to='/offices' >Offices</Link>
+                    <Link to='/help'>Help</Link>
+                </div>
+                <div className='flex gap-5 py-5 justify-center'>
+                    <div>
+                        <img className='w-7' src={instagram} alt="" />
+                    </div>
+                    <div>
+                        <img className='w-7' src={facebook} alt="" />
+                    </div>
+                    <div>
+                        <img className='w-7' src={whatsapp} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div>
+            </div>
+            <div className='text-center py-1 text-gray-500 font-semibold'>
+                Copyright @2025 - All Right Reserved
+            </div>
+        </>
+    )
+}
+
+export default Footer
