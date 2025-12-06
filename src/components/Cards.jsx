b80f0049dbef2daaae5d857de128edd8121de12d
om-dev007 <officialomnarayan@gmail.com>
2025-12-06 14:52:20 +0530

    cards updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ const Cards = ({ data }) => {
     console.log(data);
 
     return (
-        <div className="cards rounded-2xl py-2 border border-gray-300 bg-white transition-all duration-300 ease-in-out hover:translate-y-2 hover:shadow-2xl">
+        <div className="cards rounded-2xl py-2 border border-gray-300 bg-white transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-2xl">
             <div className="object-contain">
                 <img
                     className="w-50 h-50 rounded-2xl object-contain "
