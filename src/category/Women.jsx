8d6cc0009d1dac831c54c73e927853a9e81a446b
om-dev007 <officialomnarayan@gmail.com>
2025-12-07 16:23:15 +0530

    updated

--- src/category/Women.jsx
+++ src/category/Women.jsx
@@ -4,6 +4,7 @@ import creative from '../assets/summer-fashion-sale-banner-design-template-62077
 import {womenData} from '../store/data'
 import Cards from '../components/Cards'
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
 
 const Women = () => {
     return (
@@ -15,7 +16,9 @@ const Women = () => {
                 </div>
                 <div className='flex flex-wrap gap-5 px-15 justify-center py-10'>
                     {womenData.map((items) => {
-                        return <Cards data={items} key={items.id} />
+                        return <Link to={`/womens/${items.id}`}>
+                            <Cards data={items} key={items.id} />
+                        </Link>
                     })}
                 </div>
             </div>
