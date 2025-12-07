35b1952a7adb44e7bf2864d0137794732a9dd8fa
om-dev007 <officialomnarayan@gmail.com>
2025-12-07 16:22:52 +0530

    updated page

--- src/category/Kids.jsx
+++ src/category/Kids.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar'
 import { kidsData } from '../store/data'
 import Cards from '../components/Cards'
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
 
 const Kids = () => {
   return (
@@ -15,7 +16,9 @@ const Kids = () => {
         </div>
         <div className='flex flex-wrap gap-5 py-10 px-15 justify-center'>
           {kidsData.map((item) => {
-            return <Cards data={item} key={item.id} />
+            return <Link to={`/kids/${item.id}`}>
+            <Cards data={item} key={item.id} />
+            </Link>
           })}
         </div>
       </div>
