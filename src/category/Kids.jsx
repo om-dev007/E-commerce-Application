66855f7374090988c94046185f7b1540421ebc2b
om-dev007 <officialomnarayan@gmail.com>
2025-12-07 14:00:19 +0530

    updated

--- src/category/Kids.jsx
+++ src/category/Kids.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import kidsSale from '../assets/kidsSale.jpg'
 import Navbar from '../components/Navbar'
+import { kidsData } from '../store/data'
+import Cards from '../components/Cards'
+import Footer from '../components/Footer'
 
 const Kids = () => {
   return (
@@ -10,7 +13,13 @@ const Kids = () => {
         <div className='flex px-10 justify-center py-2'>
           <img className='w-[85%] rounded ' src={kidsSale} alt="" />
         </div>
+        <div className='flex flex-wrap gap-5 py-10 px-15 justify-center'>
+          {kidsData.map((item) => {
+            return <Cards data={item} key={item.id} />
+          })}
+        </div>
       </div>
+      <Footer/>
     </>
   )
 }
