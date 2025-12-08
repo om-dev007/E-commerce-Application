707b4f2e7817a811aefb45e49bf28ae6a7f9aacc
om-dev007 <officialomnarayan@gmail.com>
2025-12-08 16:14:32 +0530

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,19 +9,19 @@ import { UserRound } from 'lucide-react';
 const Navbar = () => {
   return (
     <>
-      <div className='flex flex-col sm:flex-row sm:py-2 md:flex-row items-center justify-between p-5'>
+      <div className='flex flex-col sm:flex-row sm:py-2 md:flex-row items-center justify-between px-5 py-5'>
         <div className='flex items-center gap-5 px-2 mb-4 md:mb-0'>
           <div className='flex items-center gap-1'>
             <div>
-              <img className='h-8 md:h-12' src={logoUpdated} alt="" />
+              <img className='h-10 w-15 md:w-30 sm:w-25 lg:w-30 md:h-12' src={logoUpdated} alt="" />
             </div>
             <div>
               <h1 className='font-semibold text-2xl md:text-3xl'>Velnixa</h1>
             </div>
           </div>
         </div>
-        <div className='mb-4 md:mb-0'>
-          <input className='border-2 border-gray-600 px-4 md:px-8 transition-all focus:border-green-500 py-2 rounded-2xl outline-0 w-full md:w-auto' type="text" placeholder='Search your items here...' />
+        <div className='flex justify-center mb-4 w-full md:mb-0'>
+          <input className='border-2 border-gray-600 px-4 w-3/4 md:px-8 transition-all focus:border-green-500 py-2 rounded-2xl outline-0 lg:w-3/4 md:w-3/4 ' type="text" placeholder='Search your items here...' />
         </div>
         <div className='flex justify-center'>
           <div className='flex items-center gap-3 md:gap-5'>
@@ -34,7 +34,7 @@ const Navbar = () => {
             <div>
               <button className='outline-0'>
                 <div>
-                  <img className='h-8 md:h-10' src={cart_icon} alt="" />
+                  <img className='h-8 w-8 md:w-25 lg:w-20 sm:w-20 md:h-10' src={cart_icon} alt="" />
                 </div>
               </button>
             </div>
@@ -42,7 +42,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className='flex pr-5 sm:pt-0 py-2 items-center gap-5'>
-        <div className='flex flex-wrap justify-center md:justify-evenly w-full text-gray-700 font-semibold gap-5'>
+        <div className='flex flex-wrap justify-center w-full text-gray-700 font-semibold gap-5'>
           <div>
             <Link to='/'>Home</Link>
           </div>
