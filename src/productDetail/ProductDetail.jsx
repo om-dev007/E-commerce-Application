9a6dc66d6a0f1cb6bcb946e7cc2ee34ee3501905
om-dev007 <officialomnarayan@gmail.com>
2025-12-08 19:25:34 +0530

    detailed

--- src/productDetail/ProductDetail.jsx
+++ src/productDetail/ProductDetail.jsx
@@ -2,13 +2,14 @@ import { useParams } from 'react-router-dom'
 import { womenData } from '../store/data'
 import { menData } from '../store/data'
 import { kidsData } from '../store/data'
+import { data } from '../store/data'
 import Navbar from '../components/Navbar'
 import star from '../assets/star_icon.png'
 import halfstar from '../assets/star_dull_icon.png'
 
 const ProductDetail = () => {
   const { id } = useParams()
-  const product = menData.find((item) => item.id == id) || womenData.find((item) => item.id == id) || kidsData.find((item) => item.id == id)
+  const product = menData.find((item) => item.id == id) || womenData.find((item) => item.id == id) || kidsData.find((item) => item.id == id) || data.find((item) => item.id == id)
 
   return (
     <>
@@ -19,22 +20,22 @@ const ProductDetail = () => {
           <h1 className='font-medium text-gray-500 text-center'> Home <span>-</span> Shop <span>-</span> {product.category} <span>-</span> {product.title} </h1>
         </div>
       </div>
-      <div className='flex flex-col md:flex-row gap-4 md:gap-2 px-4 md:px-15 py-5'>
-        <div className='flex flex-row md:flex-col gap-1 justify-center md:justify-start'>
+      <div className='flex flex-col sm:flex-row md:flex-row gap-4 md:gap-2 px-4 md:px-15 py-5'>
+        <div className='flex flex-row sm:flex-col order-2 md:order-1 sm:order-1 lg:order-1 md:flex-col gap-1 justify-center md:justify-start sm:justify-start '>
           <div>
-            <img className='w-20 md:w-30 rounded' src={product.image} alt="" />
+            <img className='w-12 h-10 md:w-20 md:h-15  rounded' src={product.image} alt="" />
           </div>
           <div>
-            <img className='w-20 md:w-30 rounded' src={product.image} alt="" />
+            <img className='w-12 h-10 md:w-20 md:h-15 lg:w-25 lg:h-20 rounded' src={product.image} alt="" />
           </div>
           <div>
-            <img className='w-20 md:w-30 rounded' src={product.image} alt="" />
+            <img className='w-12 h-10 md:w-20 md:h-15  rounded' src={product.image} alt="" />
           </div>
         </div>
-        <div className='rounded mx-auto md:mx-0'>
+        <div className='rounded md:order-2 sm:order-2 lg:order-2 order-1 mx-auto md:mx-0'>
           <img className='h-80 md:h-113 w-80 md:w-150 rounded object-cover' src={product.image} alt="" />
         </div>
-        <div className='flex flex-col gap-5 px-4 md:px-5 text-center md:text-left'>
+        <div className='flex order-3 sm:order-3 lg:order-3 md:order-3 flex-col gap-5 px-4 md:px-5 text-center md:text-left'>
           <div>
             <h1 className=' text-xl md:text-2xl  text-gray-900 font-semibold'> {product.title} </h1>
           </div>
