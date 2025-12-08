9fc771df95618ce078fd6a7def406ef70f36f221
om-dev007 <officialomnarayan@gmail.com>
2025-12-08 19:27:13 +0530

    new updated

--- src/pages/NewCollection.jsx
+++ src/pages/NewCollection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Cards from '../components/Cards'
 import { collectionData } from '../store/data'
+import { Link } from 'react-router-dom'
 
 const NewCollection = () => {
   return (
@@ -12,7 +13,7 @@ const NewCollection = () => {
       <div className='flex flex-wrap py-10 px-20 justify-center gap-4'>
         {
           collectionData.map((items) => {
-            return <Cards data={items} key={items.id} />
+            return <Link to={`/product/${items.id}`}> <Cards data={items} key={items.id} /> </Link>
           })
         }
       </div>
