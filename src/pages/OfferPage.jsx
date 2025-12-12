b0512613fc6f2448bbad1f3f031f65e5ba3f88d3
om-dev007 <officialomnarayan@gmail.com>
2025-12-13 00:16:37 +0530

    offerpage updated

--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -3,8 +3,8 @@ import offerGirl from '../assets/offerGirl.webp'
 
 const OfferPage = () => {
   return (
-    <div className='flex justify-center px-20  py-10 '>
-        <div className='bg-linear-to-t from-emerald-400 to-white items-center w-full justify-center md:justify-evenly rounded-2xl flex flex-col md:flex-row py-10 px-4 md:px-30'>
+    <div className='flex bg-linear-to-t from-emerald-400 to-white justify-center px-20  py-10 '>
+        <div className='items-center w-full justify-center md:justify-evenly rounded-2xl flex flex-col md:flex-row py-10 px-4 md:px-30'>
             <div className='text-center md:text-left mb-8 md:mb-0'>
                 <h1 className='text-2xl md:text-4xl font-semibold'>Exclusive</h1>
                 <h1 className='text-2xl md:text-4xl font-semibold'>Offers For You</h1>
