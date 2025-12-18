a4c7346988b63117aa7fd971106ca2eb3bab8971
om-dev007 <officialomnarayan@gmail.com>
2025-12-18 16:07:05 +0530

    cards updates

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Cards = ({ data }) => {
     return (
-        <div className="cards rounded-2xl border border-gray-100 shadow-xl bg-white/80 backdrop-blur-md transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-2xl max-w-xs mx-auto">
-            <div className="object-cover">
+        <div className="cards rounded-2xl border-b border-x  border-white shadow-2xl transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-2xl max-w-xs mx-auto">
+            <div className="object-cover overflow-hidden">
                 <img
-                    className="w-full h-48 md:h-56 lg:h-64 rounded-2xl overflow-hidden object-cover"
+                    className="w-full h-48 rounded-t-2xl md:h-56 lg:h-64 overflow-hidden object-cover"
                     src={data.image}
                     alt={data.title}
                 />
