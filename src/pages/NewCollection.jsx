1b40999385667bff7fadce390702162504cb4e54
om-dev007 <officialomnarayan@gmail.com>
2025-12-22 00:42:25 +0530

    new collection updated

--- src/pages/NewCollection.jsx
+++ src/pages/NewCollection.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Cards from '../components/Cards'
 import { collectionData } from '../store/data'
 import { Link } from 'react-router-dom'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const NewCollection = () => {
   return (
@@ -10,7 +15,29 @@ const NewCollection = () => {
         <h1 className='text-center font-bold text-4xl'>NEW COLLECTIONS</h1>
         <hr className='w-50 bg-gray-900  h-1 rounded-2xl flex justify-self-center font-extrabold' />
       </div>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-5 gap-y-5 py-10 px-8 sm:px-10 md:px-16 lg:px-20'>
+      <div className="popular-swiper-wrapper sm:hidden block py-10 px-10">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation
+          loop={true}
+          grabCursor={true}
+          speed={1000}
+          className="popular-swiper"
+        >
+          {collectionData.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className="flex flex-col h-full">
+                <Link to={`/product/${item.id}`} className="flex-1">
+                  <Cards data={item} />
+                </Link>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className='hidden sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5 py-10 px-8 sm:px-10 md:px-16 lg:px-20'>
         {
           collectionData.map((items) => {
             return <Link to={`/product/${items.id}`}> <Cards data={items} key={items.id} /> </Link>
