a8f383e74090c830133ade7bd2518c7915c862ee
om-dev007 <officialomnarayan@gmail.com>
2025-12-22 00:40:54 +0530

    cards updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,32 +2,28 @@ import React from "react";
 
 const Cards = ({ data }) => {
   return (
-    <div
-      className="
-        cards group rounded-2xl bg-black transition-all duration-300 ease-in-out hover:-translate-y-2 max-w-xs mx-auto h-full "
-    >
-      <div className="overflow-hidden rounded-t-2xl">
+    <div className="group w-full rounded-2xl bg-transparent transition-all duration-300 shadow-[0_4px_12px_rgba(0,0,0,0.08)]
+">
+      
+      <div className="aspect-6/5 overflow-hidden rounded-t-2xl">
         <img
-          className="
-            w-50 h-48 md:h-56 lg:h-64 object-cover items-start flex
-            transition-transform duration-700 ease-out
-            group-hover:scale-110
-          "
           src={data.image}
           alt={data.title}
+          className="h-full w-full object-cover transition-transform duration-700 group-hover:scale-110"
         />
       </div>
 
-      <div className="text-center py-3 px-3 text-[#e6e3dc]">
-        <h3 className="py-1 text-sm md:text-base font-semibold">
+      <div className="text-center py-3 px-3 text-gray-900">
+        <h3 className="py-1 text-sm md:text-base font-semibold line-clamp-2">
           {data.title}
         </h3>
-        <p className="text-sm md:text-base text-[#c9c5bb]">
+        <p className="text-sm md:text-base text-gray-500">
           Rating: {data.rating}
         </p>
       </div>
     </div>
   );
 };
+
 
 export default Cards;
