83fb0819a8d676fbed200c96740bb182e416d031
om-dev007 <officialomnarayan@gmail.com>
2025-12-21 23:55:52 +0530

    popular updated

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import Cards from "../components/Cards";
 import { data } from "../store/data";
 import { Link } from "react-router-dom";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -17,39 +15,15 @@ const Popular = () => {
                 <h1 className="text-3xl text-gray-800">TRENDING NOW</h1>
             </div>
 
-            {/* <div className="block sm:hidden py-10 px-4 ">
-                <Swiper
-                    modules={[Navigation]}
-                    spaceBetween={20}
-                    slidesPerView={1}
-                    navigation
-                    loop={false}
-                    grabCursor={true}
-                    touchRatio={1.3}
-                    resistanceRatio={0.85}
-                    speed={700}
-                    className="popular-swiper"
-                >
-                    {data.map((item) => (
-                        <SwiperSlide key={item.id}>
-                            <div className="bg-transparent">
-                                <Link to={`/product/${item.id}`}>
-                                    <Cards data={item} />
-                                </Link>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div> */}
             <div className="popular-swiper-wrapper sm:hidden block py-10 px-10">
                 <Swiper
                     modules={[Navigation]}
                     spaceBetween={20}
                     slidesPerView={1}
                     navigation
-                    loop={false}
+                    loop={true}
                     grabCursor={true}
-                    speed={700}
+                    speed={1000}
                     className="popular-swiper"
                 >
                     {data.map((item) => (
@@ -63,7 +37,6 @@ const Popular = () => {
                     ))}
                 </Swiper>
             </div>
-
 
             <div className="hidden sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-5 gap-y-5 py-10 px-8 sm:px-10 md:px-16 lg:px-20">
                 {data.map((item) => (
